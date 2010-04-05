f04ab5db1c492b14df70a0d138314a8bfcb555dc
Michael Schmidt <michael.j.schmidt@gmail.com>
2010-04-05 15:21:29 +0200

    nginx version back to 64

--- roles/frontend.rb
+++ roles/frontend.rb
@@ -1,4 +1,3 @@
 name "frontend"
 description "All in one Frontend. Nginx as reverse proxy, Tmocat6 as appserver, MongoDB as db"
 run_list "recipe[nginx]", "recipe[tomcat6]"
-override_attributes "nginx" => { "version" => "0.7.65" }
