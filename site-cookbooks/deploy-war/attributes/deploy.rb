231bb94200e0d42d55ab6b3c02bef6be8d3c1190
Michael Schmidt <michael.j.schmidt@gmail.com>
2010-04-11 19:35:48 +0200

    typo

--- site-cookbooks/deploy-war/attributes/deploy.rb
+++ site-cookbooks/deploy-war/attributes/deploy.rb
@@ -8,4 +8,4 @@ set_unless[:deploy][:context]    = "helloworld"
   
 set_unless[:deploy][:deploy_to]    = "/u/apps/#{deploy[:context]}"  
 set_unless[:deploy][:release_name] = Time.now.utc.strftime("%Y%m%d%H%M%S")
-set_unless[:deploy][:release_dir]  = ""#{deploy[:deploy_to]}/releases/#{deploy[:release_name]}"
+set_unless[:deploy][:release_dir]  = "#{deploy[:deploy_to]}/releases/#{deploy[:release_name]}"
