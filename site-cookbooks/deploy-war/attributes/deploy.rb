2edd353e551083c0dc505b51e245051c54973815
Michael Schmidt <michael.j.schmidt@gmail.com>
2010-04-11 19:19:20 +0200

    create releasses directory

--- site-cookbooks/deploy-war/attributes/deploy.rb
+++ site-cookbooks/deploy-war/attributes/deploy.rb
@@ -4,3 +4,7 @@ set_unless[:deploy][:module]     = "de.crashedpixels.gwt.helloworld"
 set_unless[:deploy][:revision]   = "INTEGRATION"
 set_unless[:deploy][:artifact]   = "de.crashedpixels.gwt.helloworld-INTEGRATION.war"
   
+set_unless[:deploy][:context]    = "helloworld"
+  
+set_unless[:deploy][:deploy_to]  = "/u/apps/#{deploy[:context]}"  
+  
--- site-cookbooks/deploy-war/recipes/default.rb
+++ site-cookbooks/deploy-war/recipes/default.rb
@@ -1,9 +1,19 @@
 #
 # Cookbook Name:: deploy-war
 
+directory "#{node[:deploy][:deploy_to]}/releases" do
+  mode 0755
+  owner "root"
+  group "root"
+  
+  action :create
+  recursive true
+end
+
 remote_file "war" do
   path "/tmp/#{node[:deploy][:artifact]}"
   source "http://#{node[:deploy][:bucket]}.s3.amazonaws.com/ivy/#{node[:deploy][:org]}/#{node[:deploy][:module]}/#{node[:deploy][:revision]}/#{node[:deploy][:artifact]}"
 end
+
 
 
