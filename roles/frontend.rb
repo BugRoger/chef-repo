e58d5771e9efcd57327e0c736f717e7b3fd5f5d5
Michael Schmidt <michael.j.schmidt@gmail.com>
2010-04-11 18:50:00 +0200

    added default attributes to frontend role

--- roles/frontend.rb
+++ roles/frontend.rb
@@ -1,3 +1,9 @@
 name "frontend"
 description "All in one Frontend. Nginx as reverse proxy, Tmocat6 as appserver, MongoDB as db"
-run_list "recipe[nginx]", "recipe[tomcat]"
+run_list "recipe[nginx]", "recipe[tomcat]", "recipe[deploy-war]"
+default_attributes "deploy-war" => { 
+  "org"      => "ca.sap.com",
+  "module"   => "de.crashedpixels.gwt.helloworld",
+  "revision" => "INTEGRATION",
+  "artifact" => "de.crashedpixels.gwt.helloworld-INTEGRATION.war"
+}
